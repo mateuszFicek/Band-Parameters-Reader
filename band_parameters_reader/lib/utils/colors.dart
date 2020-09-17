c365c16d7801a3cb44f31806fc5fd8be376c665e
mateuszFicek <mateuszficek295@gmail.com>
2020-09-17 15:20:50 +0200

    Constant files added

---
+++ band_parameters_reader/lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class UIColors {
+  static const Color BACKGROUND_COLOR = Color(0xFF0E1821);
+  static const Color TILE_BACKGROUND_COLOR = Color(0xFF124FE0);
+
+  static const Color LIGHT_FONT_COLOR = Color(0xFFD4E1ED);
+}
