910f6f64a17bb2a6614b557eecf9220d6998b8af
mateuszFicek <mateuszficek295@gmail.com>
2020-09-17 15:21:22 +0200

    Google fonts and screen utils plugins added

--- band_parameters_reader/lib/main.dart
+++ band_parameters_reader/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:band_parameters_reader/ui/main/main.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'di/provider_container.dart';
 
@@ -14,6 +16,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
       title: 'Band Parameters Reader',
       routes: {
