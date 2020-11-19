fcba12c985fe0bae78df7aafea88743dafa4e0ba
Mateusz Ficek <mateusz.ficek@softnauts.com>
2020-11-19 19:11:01 +0100

    Separated ble/bth screen

---
+++ band_parameters_reader/lib/ui/main/bluetooth_devices.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BluetoothDevices extends StatefulWidget {
+  BluetoothDevices({Key key}) : super(key: key);
+
+  @override
+  _BluetoothDevicesState createState() => _BluetoothDevicesState();
+}
+
+class _BluetoothDevicesState extends State<BluetoothDevices> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ band_parameters_reader/lib/ui/main/bluetooth_l_e_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BluetoothLEWidget extends StatefulWidget {
+  BluetoothLEWidget({Key key}) : super(key: key);
+
+  @override
+  _BluetoothLEWidgetState createState() => _BluetoothLEWidgetState();
+}
+
+class _BluetoothLEWidgetState extends State<BluetoothLEWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
